f331cfc9f3231842bc2d0b554069ae99c88e85de
Jamie Conway <jamie@jpconway.com>
2023-08-09 09:19:32 -0500

    set width and height to be the same value

--- src/components/CircleIconsSubMenu.tsx
+++ src/components/CircleIconsSubMenu.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-const images = ['about-us', 'blog', 'newsletter', 'work-with-us']
+const images = ['about', 'blog', 'newsletter', 'work-with-us']
 
 const CircleIconsSubMenu = () => (
     <div className="flex flex-col md:flex-row items-center justify-evenly w-full py-32">
@@ -15,7 +15,7 @@ const CircleIconsSubMenu = () => (
                 className='py-10 md:py-0' 
                 src={`/images/circle-icon-${imageName}.png`}
                 width={200}
-                height={45}
+                height={200}
                 alt="Nourished"
             />
             </Link>
