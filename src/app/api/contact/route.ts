7e9e6dba46cf9dec45323c7729c606b38c1bde3a
Jamie Conway <jamie@jpconway.com>
2023-08-08 23:24:48 -0500

    add contact api route

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,23 @@
+// route to handle contact form submission
+
+import { sendMail } from "@/lib/sendMail"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+    const formData = await req.json()
+
+    const { firstName, lastName, email, message } = formData
+    
+    if (req.method !== "POST") {
+        return NextResponse.json({ message: "Method not allowed" })
+    }
+    
+    if (!firstName || !lastName || !email || !message) {
+        return NextResponse.json({ message: "Invalid input" })
+    }
+
+    await sendMail({name:`${firstName} ${lastName}`, email, message}).catch(console.error)
+    
+    return NextResponse.json({ formData })
+
+}
