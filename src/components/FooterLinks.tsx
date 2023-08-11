58510c816be12780b31372d4c3353f8d02ddf815
Jamie Conway <jamie@jpconway.com>
2023-08-11 16:55:14 -0500

    deactivate invalid pages

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -10,16 +10,16 @@ const transatBold = localFont({
 const FooterLinks = () => (
     <div className={`${transatBold.className} pt-32 flex flex-col sm:flex-row items-top justify-between text-center text-2xl text-nbrg-500 uppercase gap-8 w-screen max-w-7xl`}>
         <span className='w-full sm:w-screen-1/3 flex flex-col items-center justify-start gap-6'>
-            <Link className="border-b-2 border-b-nbrg-500" href="/about-us">About</Link>
-            <Link className="border-b-2 border-b-nbrg-500" href="/blog">Blog</Link>
-            <Link className="border-b-2 border-b-nbrg-500" href="/newsletter">Newsletter</Link>
-            <Link className="border-b-2 border-b-nbrg-500" href="/contact">Contact</Link>
+            <Link  href="/about"><button className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">About</button></Link>
+            <Link  href="/blog"><button disabled className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Blog</button></Link>
+            <Link  href="/newsletter"><button disabled className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Newsletter</button></Link>
+            <Link  href="/contact"><button className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Contact</button></Link>
         </span>
         <span className='w-full sm:w-screen-1/3 flex flex-col items-center justify-start gap-6'>
-            <Link className="border-b-2 border-b-nbrg-500" href="/work-with-us">Work With Us</Link>
-            <Link className="border-b-2 border-b-nbrg-500" href="/body-wisdom-mini-course">Online Classes</Link>
-            <Link className="border-b-2 border-b-nbrg-500" href="/body-wisdom-mini-course">Body Wisdom Mini-Course</Link>
-            <Link className="border-b-2 border-b-nbrg-500" href="/call">Book A Discovery Call</Link>
+            <Link  href="/work-with-us"><button disabled className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Work With Us</button></Link>
+            <Link  href="/body-wisdom-mini-course"><button disabled className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Online Classes</button></Link>
+            <Link  href="/body-wisdom-mini-course"><button disabled className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Body Wisdom Mini-Course</button></Link>
+            <Link  href="/call"><button disabled className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Book A Discovery Call</button></Link>
         </span>
     </div>
 )
