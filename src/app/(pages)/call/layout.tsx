e9e94e78e5b44077a95032e1880b2a6f540aeef7
Jamie Conway <jamie@jpconway.com>
2023-08-22 11:35:21 -0500

    switch back to iframe & remove script

--- src/app/(pages)/call/layout.tsx
+++ src/app/(pages)/call/layout.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Script from "next/script"
 import React from "react"
 
 export default function RootLayout({
@@ -16,22 +15,27 @@ export default function RootLayout({
                 {children}
                 {/* <!-- Practice Better Booking Widget: start --> */}
                 <div 
-                    className="better-inline-booking-widget relative flex flex-col items-center overflow-hidden w-full max-w-3xl h-screen" 
+                    className="better-inline-booking-widget relative flex flex-col items-center overflow-hidden w-full max-w-3xl" 
                     data-url="https://my.practicebetter.io" 
                     data-service="64527318ed3b9cd1ae61ff22" 
                     data-hash="6452605f4c15a8e1b30dd2df" 
                     data-theme="789d4a" 
                     data-theme-accent="f94525" 
                     data-scrollbar-visible="false"
+                    style={{
+                        minHeight:"800px",
+                    }}
                 >
-                    
+                    <iframe 
+                        className="absolute inset-0 w-full h-full border-0 px-2 pt-8 flex flex-col items-center ml-auto mr-auto"
+                        src="https://my.practicebetter.io?fl_wtc=789D4A&amp;fl_wtac=F94525#/6452605f4c15a8e1b30dd2df/widgets/bookings?s=64527318ed3b9cd1ae61ff22" 
+                        style={{
+                            maxWidth:"550px",
+                            minHeight:"fit-content",
+                        }}>    
+                    </iframe>
                 </div>
-                <Script 
-                    strategy="lazyOnload"
-                    type="text/javascript"
-                    src="https://cdn.practicebetter.io/assets/js/booking.widget.js" 
-                    async
-                />
+                
                 {/* <!-- Practice Better Booking Widget: end --> */}
             </section>
         </>
