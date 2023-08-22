9554e0b358928c83f7d10769aa782e02e2c63c8e
Jamie Conway <jamie@jpconway.com>
2023-08-21 20:43:40 -0500

    update variant and align on main image component

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
 <>
     <main className="top-0 w-screen flex min-h-screen flex-col items-center justify-start">
-        <FullWidthImageBehindGradient variant=""><p className={`${transatBold.className} w-screen text-center uppercase`}>Functional Food Coaching</p></FullWidthImageBehindGradient>
+        <FullWidthImageBehindGradient variant="primary" align="right"><p className={`${transatBold.className} w-screen text-center uppercase`}>Functional Food Coaching</p></FullWidthImageBehindGradient>
         <FullWidthColorBackground variant="white" textColor="secondary">
             We&apos;re Jamie and Lindsey. We teach people how to build confidence in their kitchens and optimize their health habits so they can feel better in their bodies.
         </FullWidthColorBackground>
