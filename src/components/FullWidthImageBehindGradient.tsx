b3de604be9e6ab437632413adafa0f7754b6ad03
Jamie Conway <jamie@jpconway.com>
2023-08-22 14:47:27 -0500

    update how image className is handled

--- src/components/FullWidthImageBehindGradient.tsx
+++ src/components/FullWidthImageBehindGradient.tsx
@@ -27,21 +27,38 @@ type gradientColorsType = {
 const FullWidthImageBehindGradient: React.FC<OuterProps> = ({variant, imageUrl, align, children}) => {
 
     const colors: gradientColorsType = {
-        primary: ' from-nblg-500 from-10% via-ndag-500 via-10% to-transparent to-75% dark:from-ndag-700 dark:from-5% dark:via-ndag-500 dark:via-10% dark:to-transparent ',
-        secondary: ' from-nbrg-500 from-10% via-nbrg-500 via-10% to-transparent to-75% dark:from-nbrg-700 dark:from-5% dark:via-nbrg-500 dark:via-10% dark:to-transparent ',
-        tertiary: ' from-npag-500 from-10% via-npag-500 via-10% to-transparent to-75% dark:from-npag-700 dark:from-5% dark:via-npag-500 dark:via-10% dark:to-transparent ',
-        dark: ' from-ndag-500 from-10% via-ndag-500 via-10% to-transparent to-75% dark:from-ndag-700 dark:from-5% dark:via-ndag-500 dark:via-10% dark:to-transparent ',
+        primary: 'from-nblg-500 from-10% via-nblg-500 via-15% to-transparent to-75% dark:from-ndag-700 dark:from-5% dark:via-ndag-500 dark:via-10% dark:to-transparent',
+        secondary: 'from-nbrg-500 from-10% via-nbrg-500 via-15% to-transparent to-75% dark:from-nbrg-700 dark:from-5% dark:via-nbrg-500 dark:via-10% dark:to-transparent',
+        tertiary: 'from-npag-500 from-10% via-npag-500 via-15% to-transparent to-75% dark:from-npag-700 dark:from-5% dark:via-npag-500 dark:via-10% dark:to-transparent',
+        dark: 'from-ndag-500 from-10% via-ndag-500 via-15% to-transparent to-75% dark:from-ndag-700 dark:from-5% dark:via-ndag-500 dark:via-10% dark:to-transparent',
         white: 'white',
         transparent: 'transparent'
     }
     const aligns = ['left', 'center', 'right']
     const color = colors[variant] ? colors[variant] : colors['primary']
-    const aligned = align && aligns.includes(align) ? `object-${align}` : 'object-center'
+
+    interface imageClassNamesType {
+        [key: string]: string
+    }
+
+    const imageClassNames: imageClassNamesType = {
+        left: "absolute inset-0 w-full h-full object-cover object-left",
+        center: "absolute inset-0 w-full h-full object-cover object-center",
+        right: "absolute inset-0 w-full h-full object-cover object-right",
+    }
+
+    let imageClassName = ""
+
+    if (align && aligns.includes(align)) {
+        imageClassName = imageClassNames[align]
+    } else {
+        imageClassName = imageClassNames['center']
+    }
 
     return (
     <div className="relative h-screen-2/3 w-full">
         <Image
-            className={`absolute inset-0 w-full h-full object-cover ${aligned}`}
+            className={imageClassName}
             src={imageUrl || "/images/NourishedCoJamieandLindsey-04.jpg"}
             width={1800}
             height={989}
