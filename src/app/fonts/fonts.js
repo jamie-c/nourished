c42883795c1ae774d008127d8d411b4754b6d156
Jamie Conway <jamie@jpconway.com>
2023-08-23 11:39:48 -0500

    create font exports

---
+++ src/app/fonts/fonts.js
@@ -0,0 +1,56 @@
+import localFont from "next/font/local";
+
+export const transatLight = localFont({
+    src: './transat/TransatLight.ttf',
+    display: 'swap',
+});
+
+export const transatLightOblique = localFont({
+    src: './transat/TransatLightOblique.ttf',
+    display: 'swap',
+});
+
+export const transatStandard = localFont({
+    src: './transat/TransatStandard.ttf',
+    display: 'swap',
+});
+
+export const transatStandardOblique = localFont({
+    src: './transat/TransatStandardOblique.ttf',
+    display: 'swap',
+});
+
+export const transatMedium = localFont({
+    src: './transat/TransatMedium.ttf',
+    display: 'swap',
+});
+
+export const transatMediumOblique = localFont({
+    src: './transat/TransatMediumOblique.ttf',
+    display: 'swap',
+});
+
+export const transatBold = localFont({
+    src: './transat/TransatBold.ttf',
+    display: 'swap',
+});
+
+export const transatBoldOblique = localFont({
+    src: './transat/TransatBoldOblique.ttf',
+    display: 'swap',
+});
+
+export const transatBlack = localFont({
+    src: './transat/TransatBlack.ttf',
+    display: 'swap',
+});
+
+export const transatBlackOblique = localFont({
+    src: './transat/TransatBlackOblique.ttf',
+    display: 'swap',
+});
+
+export const chaletNY1970 = localFont({
+    src: './chaletnewyorknineteenseventy.ttf',
+    display: 'swap',
+});
