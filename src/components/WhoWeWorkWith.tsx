e48d41491c10cfb0ee9466c57d15a34e962dd153
Jamie Conway <jamie@jpconway.com>
2023-08-25 16:42:47 -0500

    update padding and justify some text

--- src/components/WhoWeWorkWith.tsx
+++ src/components/WhoWeWorkWith.tsx
@@ -4,8 +4,8 @@ import Image from "next/image"
 
 const WhoWeWorkWith = () => (
     <>
-    <h2 className={`${transatBold.className} text-4xl text-center uppercase mt-32 mb-16 text-ndag-500`}>WHO WE WORK WITH</h2>
-    <div className="flex flex-col sm:flex-row items-start content-center gap-20">
+    <h2 className={`${transatBold.className} text-4xl text-center uppercase mt-32 mb-16 text-ndag-500 px-16 sm:px-4`}>WHO WE WORK WITH</h2>
+    <div className="flex flex-col sm:flex-row items-start content-center gap-20 px-4">
         <div id="work-with-individuals" className="flex flex-col items-center max-w-xs text-center text-nbrg-500">
             <Image
                 className='mb-4' 
@@ -15,7 +15,7 @@ const WhoWeWorkWith = () => (
                 alt="Individuals Seeking Health and Wellness"
             />
             <h2 className={`${transatBold.className} uppercase text-3xl`}>INDIVIDUALS SEEKING HEALTH & FOOD-CHANGE</h2>
-            <p className={`${transatLight.className} my-16 text-2xl`}>With our combined knowledge of the body&apos;s systems and food, we help people optimize how their bodies function, working with diet and lifestyle changes that lead to impactful, sustainable health change. Every recommendation is made with the individual in mind. This is not about fixed plans or one-size-fits-all approaches — it&apos;s a whole-person approach to health.</p>
+            <p className={`${transatLight.className} my-16 text-2xl text-justify`}>With our combined knowledge of the body&apos;s systems and food, we help people optimize how their bodies function, working with diet and lifestyle changes that lead to impactful, sustainable health change. Every recommendation is made with the individual in mind. This is not about fixed plans or one-size-fits-all approaches — it&apos;s a whole-person approach to health.</p>
         </div>
         <div id="work-with-professionals" className="flex flex-col items-center max-w-xs text-center text-nblg-500">
             <Image
@@ -26,7 +26,7 @@ const WhoWeWorkWith = () => (
                 alt="Other Health and Wellness Professionals"
             />
             <h2 className={`${transatBold.className} uppercase text-3xl`}>OTHER HEALTH & WELLNESS PROFESSIONALS</h2>
-            <p className={`${transatLight.className} my-16 text-2xl`}>Over the past decade, we&apos;ve helped people navigate health challenges due to immune, weight, digestive, and energy issues (to name just a few!). We&apos;ve consulted with medical professionals in need of food support for their patients. We&apos;ve served over 10,000 people our one-of-a-kind recipes through our plant-based, allergen-friendly restaurant in Oklahoma City. </p>
+            <p className={`${transatLight.className} my-16 text-2xl text-justify`}>Over the past decade, we&apos;ve helped people navigate health challenges due to immune, weight, digestive, and energy issues (to name just a few!). We&apos;ve consulted with medical professionals in need of food support for their patients. We&apos;ve served over 10,000 people our one-of-a-kind recipes through our plant-based, allergen-friendly restaurant in Oklahoma City. </p>
         </div>
     </div>
     </>
