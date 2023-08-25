1ab3d9c5938eb7928cd598ec0880c636d979733e
Jamie Conway <jamie@jpconway.com>
2023-08-25 16:43:20 -0500

    add padding on xs and justify text

--- src/components/WhoWeWorkWithV2.tsx
+++ src/components/WhoWeWorkWithV2.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 const WhoWeWorkWithV2 = () => (
     <>
-    <h2 className={`${transatBold.className} text-4xl text-center uppercase mb-16 text-ndag-500`}>WHO WE WORK WITH</h2>
+    <h2 className={`${transatBold.className} text-4xl text-center uppercase mb-16 text-ndag-500 px-16 sm:px-4`}>WHO WE WORK WITH</h2>
     <div className="flex flex-col sm:flex-row items-start content-center gap-20">
         <div id="work-with-individuals" className="flex flex-col items-center max-w-xs text-center text-nbrg-500">
             <Image
@@ -14,7 +14,7 @@ const WhoWeWorkWithV2 = () => (
                 height={300}
                 alt="Individuals Seeking Health and Wellness"
             />
-            <p className={`${transatLight.className} my-16 text-2xl`}>Are you <span className={transatMedium.className}>overwhelmed, stuck, or tired</span> of trying to find sustainable solutions to your health challenges? Whether they be digestive issues, lack of energy, brain fog, weight concerns, or autoimmunity. If you&apos;re ready to be proactive in improving health we&apos;re here to support you!</p>
+            <p className={`${transatLight.className} my-16 text-2xl text-justify`}>Are you <span className={transatMedium.className}>overwhelmed, stuck, or tired</span> of trying to find sustainable solutions to your health challenges? Whether they be digestive issues, lack of energy, brain fog, weight concerns, or autoimmunity. If you&apos;re ready to be proactive in improving health we&apos;re here to support you!</p>
         </div>
         <div id="work-with-professionals" className="flex flex-col items-center max-w-xs text-center text-nblg-500">
             <Image
@@ -24,7 +24,7 @@ const WhoWeWorkWithV2 = () => (
                 height={300}
                 alt="Other Health and Wellness Professionals"
             />
-            <p className={`${transatLight.className} my-16 text-2xl`}>Healthcare providers who are seeking functional-nutrition education and support for their patients. We help fill the gap between appointments, working with your patients to improve their everyday habits to create better health outcomes and stronger partnerships.</p>
+            <p className={`${transatLight.className} my-16 text-2xl text-justify`}>Healthcare providers who are seeking functional-nutrition education and support for their patients. We help fill the gap between appointments, working with your patients to improve their everyday habits to create better health outcomes and stronger partnerships.</p>
         </div>
     </div>
     </>
