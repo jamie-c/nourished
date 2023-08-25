cbcbca7dd636038b90df6930c1a39a669577a2b8
Jamie Conway <jamie@jpconway.com>
2023-08-24 21:49:57 -0500

    enable blog link

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -11,7 +11,7 @@ const FooterLinks = () => (
     <div className={`${transatBold.className} pt-32 flex flex-col sm:flex-row items-top justify-between text-center text-2xl text-nbrg-500 uppercase gap-8 w-screen max-w-7xl`}>
         <span className='w-full sm:w-screen-1/3 flex flex-col items-center justify-start gap-6'>
             <Link  href="/about"><button className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">About</button></Link>
-            <Link  href="/blog"><button disabled className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Blog</button></Link>
+            <Link  href="/blog"><button className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Blog</button></Link>
             <Link  href="/newsletter"><button className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Newsletter</button></Link>
             <Link  href="/contact"><button className="border-b-2 border-b-nbrg-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">Contact</button></Link>
         </span>
