de7f754ab4114670914037f1e8868e1795f215d5
Jamie Conway <jamie@jpconway.com>
2023-08-25 16:40:46 -0500

    update to accept child text for button and href prop

--- src/components/CTAFeelNourished.tsx
+++ src/components/CTAFeelNourished.tsx
@@ -2,12 +2,12 @@ import { chaletNY1970 } from '@/app/fonts/fonts'
 import StyledButton from './StyledButton'
 
 
-const CTAFeelNourished = () => (
+const CTAFeelNourished = ({ children, href }: { children: React.ReactNode, href?: string }) => (
     <>
         <p className='text-3xl sm:text-4xl mb-10'>LET&apos;S FIGURE OUT WHAT YOUR BODY NEEDS TO</p>
         <p className={`${chaletNY1970.className} text-npag-500 text-3xl sm:text-4xl mb-10 tracking-wide`}>FEEL NOURISHED.</p>
-        <StyledButton href='/work-with-us' variant='tertiary'>
-            I WANT TO LEARN MORE
+        <StyledButton href={href || "/work-with-us"} variant='tertiary'>
+            {children}
         </StyledButton>
     </>
 )
