553b4dfabd99cbdc95634022adaa125cf56408e3
Jamie Conway <jamie@jpconway.com>
2023-09-14 11:16:38 -0500

    create body-wisdom route

---
+++ src/app/(pages)/body-wisdom/page.tsx
@@ -0,0 +1,13 @@
+function Page() {
+    return (
+        <div className="min-h-screen w-screen flex flex-col items-center">
+            <div className="max-w-7xl w-screen sm:w-7xl pl-4 pt-16">
+                <h1 className="text-3xl mb-10">
+                    Welcome to the Body Wisdom Course!
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Page
