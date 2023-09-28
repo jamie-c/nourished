2c47b140b4567c6b00150c48e7b417ba84191bb8
Jamie Conway <jamie@jpconway.com>
2023-09-28 09:24:30 -0500

    add SessionProvider to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import Copyright from '@/components/Copyright'
-import FooterLinks from '@/components/FooterLinks'
-import type { Metadata } from 'next'
-import { transatStandard } from './fonts/fonts'
-import './globals.css'
+import Copyright from "@/components/Copyright"
+import FooterLinks from "@/components/FooterLinks"
+import type { Metadata } from "next"
+import ProviderWrapper from "./ProviderWrapper"
+import { transatStandard } from "./fonts/fonts"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: 'Nourished Co',
-  description: 'Functional Nutrition Consulting',
+    title: "Nourished Co",
+    description: "Functional Nutrition Consulting",
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={`${transatStandard.className}`}>
-        {children}
-        <footer className="pb-32 w-screen flex flex-col items-center justify-start">
-          <Copyright />
-          <FooterLinks />
-        </footer>
-      </body>
-
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={`${transatStandard.className}`}>
+                <ProviderWrapper>
+                    {children}
+                    <footer className="pb-32 w-screen flex flex-col items-center justify-start">
+                        <Copyright />
+                        <FooterLinks />
+                    </footer>
+                </ProviderWrapper>
+            </body>
+        </html>
+    )
 }
