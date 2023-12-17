534cb3442d9ed94a7dde4b861591d2bb81a45aab
Jamie Conway <jamie.c@nodecraft.com>
2023-12-16 22:27:17 -0600

    update nav link to reflect logged in state

--- src/components/NFCNavLink/NFCNavLink.tsx
+++ src/components/NFCNavLink/NFCNavLink.tsx
@@ -1,32 +1,52 @@
 "use client"
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+import { useEffect, useState } from "react";
 import type { NavLinkProps } from "./NFCNavLinkTypes";
 
 const NFCNavLink: React.FC<NavLinkProps> = ({ url, disabled, title }) => {
-  const pathName = usePathname();
-  const active = pathName === url;
-  const isDisabled = disabled ? "opacity-50 cursor-not-allowed " : "";
+    const { data: session, update } = useSession()
+    const pathName = usePathname();
+    const active = pathName === url;
+    const isDisabled = disabled ? "opacity-50 cursor-not-allowed " : "";
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <div className="h-full relative flex items-center">
-      <Link
-        className={`${
-          isDisabled
-        } h-full uppercase text-white font-transat-bold translate-y-0.5 relative peer flex items-center`}
-        href={url}
-      >
-        {title}
-      </Link>
-      {!disabled && !active && (
-        <span className="absolute bottom-5 w-0 h-0.5 bg-white transition-width duration-300 ease peer-hover:w-full"></span>
-      )}
-      {active && (
-        <span className="absolute bottom-5 w-full h-0.5 bg-white transition-all duration-300 ease"></span>
-      )}
-    </div>
-  );
+    const [liveTitle, setLiveTitle] = useState(title);
+
+    useEffect(() => {
+        if (session) {
+            setIsLoggedIn(true);
+        }
+    }, [session]);
+
+    useEffect(() => {
+        if (title === "Login" && isLoggedIn) {
+            setLiveTitle("Account");
+        } else {
+            setLiveTitle(title);
+        }
+    }, [isLoggedIn, title]);
+
+    return (
+        <div className="h-full relative flex items-center">
+            <Link
+                className={`${
+                    isDisabled
+                } h-full uppercase text-white font-transat-bold translate-y-0.5 relative peer flex items-center`}
+                href={url}
+            >
+                {liveTitle}
+            </Link>
+            {!disabled && !active && (
+                <span className="absolute bottom-5 w-0 h-0.5 bg-white transition-width duration-300 ease peer-hover:w-full"></span>
+            )}
+            {active && (
+                <span className="absolute bottom-5 w-full h-0.5 bg-white transition-all duration-300 ease"></span>
+            )}
+        </div>
+    );
 };
 
 export default NFCNavLink;
