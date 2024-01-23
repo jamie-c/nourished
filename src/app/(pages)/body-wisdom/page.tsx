d27db92c86d577981875956980a59f922abbd21a
Jamie Conway <jamie.c@nodecraft.com>
2024-01-23 07:29:30 -0600

    add session check

--- src/app/(pages)/body-wisdom/page.tsx
+++ src/app/(pages)/body-wisdom/page.tsx
@@ -3,21 +3,24 @@ import { useSession } from "next-auth/react"
 
 function Page() {
     const { data: session } = useSession()
-    return (
-        <div className="min-h-screen w-screen flex flex-col items-center">
-            <div className="max-w-7xl w-screen sm:w-7xl pl-4 pt-16">
-                <h1 className="text-3xl mb-10">
-                    Welcome to the Body Wisdom Course!
-                </h1>
-                {session && (
-                    <>
-                        <p>{session?.user.name}</p>
-                        <p>{session?.user.email}</p>
-                    </>
-                )}
+    if (!session) return (<div>Please sign in to view this content...</div>)
+    if (session.user) {
+        return (
+            <div className="min-h-screen w-screen flex flex-col items-center">
+                <div className="max-w-7xl w-screen sm:w-7xl pl-4 pt-16">
+                    <h1 className="text-3xl mb-10">
+                        Welcome to the Body Wisdom Course!
+                    </h1>
+                    {session && (
+                        <>
+                            <p>{session?.user.name}</p>
+                            <p>{session?.user.email}</p>
+                        </>
+                    )}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Page
