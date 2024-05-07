d2a5a2f0a5b0af9c8867d75f34109cd7f7ac1f78
Jamie Conway <jamie.c@nodecraft.com>
2024-05-07 07:38:34 -0500

    refactor: logo to ui

--- src/components/NFCHeader/NFCHeader.tsx
+++ src/components/NFCHeader/NFCHeader.tsx
@@ -1,13 +1,13 @@
-import NFCLogo from "@/components/NFCLogo/NFCLogo";
+import { NFCLogo } from "@nourishedco/ui";
 import { NFCNavLinksHorizontal } from "../NFCNavLinks/NFCNavLinks";
 
 const Header = () => {
 
   const headerClassNames = {
     navLinksHidden:
-      "sticky top-0 w-screen z-40 bg-opacity-50 dark:bg-opacity-50 bg-nblg-500 dark:bg-nblg-700 backdrop-blur-xl dark:backdrop-blur-xl transition-all duration-300",
+      "sticky top-0 w-screen z-40 bg-opacity-50 dark:bg-opacity-50 bg-bluegreen-500 dark:bg-bluegreen-700 backdrop-blur-xl dark:backdrop-blur-xl transition-all duration-300",
     navLinksVisible:
-      "fixed top-0 w-screen h-screen z-40 bg-opacity-50 dark:bg-opacity-50 bg-nblg-500 dark:bg-nblg-700 backdrop-blur-xl dark:backdrop-blur-xl transition-all duration-300",
+      "fixed top-0 w-screen h-screen z-40 bg-opacity-50 dark:bg-opacity-50 bg-bluegreen-500 dark:bg-bluegreen-700 backdrop-blur-xl dark:backdrop-blur-xl transition-all duration-300",
   };
 
   return (
@@ -15,7 +15,7 @@ const Header = () => {
       className={`drop-shadow-xl ${headerClassNames.navLinksHidden}`}
     >
       <div className="px-7 flex flex-row items-center justify-between max-w-7xl h-16 m-auto">
-        <NFCLogo />
+        <NFCLogo imgUrl="/images/NOURISHED_LOGO_NOTAG_WHITE.png"/>
         <div className="h-full hidden md:flex">
           <NFCNavLinksHorizontal />
         </div>
