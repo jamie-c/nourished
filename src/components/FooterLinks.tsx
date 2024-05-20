829141639663752442a103344427b7064a69352b
Jamie Conway <jamie.c@nodecraft.com>
2024-05-20 06:54:10 -0500

    typo: color name misspelled

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -12,44 +12,68 @@ const FooterLinks = () => (
 	>
 		<span className="w-full sm:w-screen-1/3 flex flex-col items-center justify-start gap-6">
 			<Link href="/about">
-				<button className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					About
 				</button>
 			</Link>
 			<Link href="/blog">
-				<button className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					Blog
 				</button>
 			</Link>
 			<Link href="/newsletter">
-				<button className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					Newsletter
 				</button>
 			</Link>
 			<Link href="/contact">
-				<button className="border-b-2 border-b-brinbrgghtgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					Contact
 				</button>
 			</Link>
 		</span>
 		<span className="w-full sm:w-screen-1/3 flex flex-col items-center justify-start gap-6">
 			<Link href="/work-with-us">
-				<button className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					Work With Us
 				</button>
 			</Link>
 			<Link href="/body-wisdom">
-				<button className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					Online Classes
 				</button>
 			</Link>
 			<Link href="/body-wisdom/course-content">
-				<button className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					Body Wisdom Mini-Course
 				</button>
 			</Link>
 			<Link href="/call">
-				<button className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed">
+				<button
+					type="button"
+					className="border-b-2 border-b-brightgreen-500 uppercase disabled:opacity-50 disabled:cursor-not-allowed"
+				>
 					Book A Discovery Call
 				</button>
 			</Link>
