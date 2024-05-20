603f30dacb75c8319434e6ed019285d7f38408e5
Jamie Conway <jamie.c@nodecraft.com>
2024-05-20 06:42:05 -0500

    fix: nav links functional

--- src/components/NFCNavLinks/NFCNavLinks.tsx
+++ src/components/NFCNavLinks/NFCNavLinks.tsx
@@ -1,88 +1,88 @@
-import NavLink from "@/components/NavLink/NavLink"
+import NavLink from "@/components/NavLink/NavLink";
 
-import type { NavLinks } from "@/components/NFCNavLinks/NFCNavLinksTypes"
-import UserAccountIcon from "../UserAccountIcon"
+import type { NavLinks } from "@/components/NFCNavLinks/NFCNavLinksTypes";
+import UserAccountIcon from "../UserAccountIcon";
 
 const mainNavItems: NavLinks[] = [
-    {
-        title: "About",
-        url: "/about",
-        id: "about",
-    },
-    {
-        title: "Work With Us",
-        url: "/work-with-us",
-        id: "work-with-us",
-    },
-    {
-        title: "Blog",
-        url: "/blog",
-        id: "blog",
-    },
-    {
-        title: "Contact",
-        url: "/contact",
-        id: "contact",
-    },
-]
+	{
+		title: "About",
+		url: "/about",
+		id: "about",
+	},
+	{
+		title: "Work With Us",
+		url: "/work-with-us",
+		id: "work-with-us",
+	},
+	{
+		title: "Blog",
+		url: "/blog",
+		id: "blog",
+	},
+	{
+		title: "Contact",
+		url: "/contact",
+		id: "contact",
+	},
+];
 
 export const NFCNavLinksHorizontal = () => {
-    const navItems = mainNavItems.reduce(
-        (acc: NavLinks[], { title, url, id }, i, a) => {
-            if (i < a.length - 1) {
-                acc.push({ title, url, id })
-                acc.push({ title: "divider", url: "", id: `divider-${i}` })
-            }
-            if (i === a.length - 1) {
-                acc.push({ title, url, id })
-            }
-            return acc
-        },
-        []
-    )
+	const navItems = mainNavItems.reduce(
+		(acc: NavLinks[], { title, url, id }, i, a) => {
+			if (i < a.length - 1) {
+				acc.push({ title, url, id });
+				acc.push({ title: "divider", url: "", id: `${id}-divider` });
+			}
+			if (i === a.length - 1) {
+				acc.push({ title, url, id });
+			}
+			return acc;
+		},
+		[],
+	);
 
-    return (
-        <span className="h-full">
-            <nav className="flex-10 flex flex-row items-center justify-end gap-3 text-xl text-white first:before:content-none h-full">
-                {navItems.map(({ title, url, id }, i, a) => {
-                    if (title !== "divider") {
-                        return <NavLink key={id} url={url} title={title} />
-                    }
-                    if (title === "divider") {
-                        return (
-                            <span
-                                key={id}
-                                className="bg-white h-1.5 rounded-full w-1.5"
-                            />
-                        )
-                    }
-                    return
-                })}
-                <span className="bg-white h-1.5 rounded-full w-1.5" />
-                <UserAccountIcon />
-            </nav>
-        </span>
-    )
-}
+	return (
+		<span className="h-full">
+			<nav className="flex-10 flex flex-row items-center justify-end gap-3 text-xl text-white first:before:content-none h-full">
+				{navItems.map(({ title, url, id }) => {
+					if (title !== "divider") {
+						return <NavLink key={id} url={url} title={title} />;
+					}
+					if (title === "divider") {
+						return (
+							<span
+								key={`${id}-divider`}
+								className="bg-white h-1.5 rounded-full w-1.5"
+							/>
+						);
+					}
+					return;
+				})}
+				<span className="bg-white h-1.5 rounded-full w-1.5" />
+				<UserAccountIcon />
+			</nav>
+		</span>
+	);
+};
 
 export const NFCNavLinksVertical = () => {
-    return (
-        <span>
-            <nav className="flex flex-col justify-start items-end gap-0 text-4xl text-white pt-10 pr-8">
-                {mainNavItems.map(({ title, url }, i) => {
-                    if (title !== "divider") {
-                        return (
-                            <div key={title} className="h-20">
-                                <NavLink title={title} url={url} disabled={false} />
-                            </div>
-                        )
-                    }
-                    if (title === "divider") {
-                        return
-                    }
-                    return
-                })}
-            </nav>
-        </span>
-    )
-}
+	return (
+		<span>
+			<nav className="flex flex-col justify-start items-end gap-0 text-4xl text-white pt-10 pr-8">
+				{mainNavItems.map(({ title, url }, i) => {
+					if (title !== "divider") {
+						return (
+							<div key={title} className="h-20">
+								<NavLink title={title} url={url} disabled={false} />
+							</div>
+						);
+					}
+					if (title === "divider") {
+						return;
+					}
+					return;
+				})}
+			</nav>
+		</span>
+	);
+};
--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { NFCNavLink } from "@nourishedco/ui";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import type { NavLinkProps } from "./NavLinkTypes";
@@ -15,8 +15,27 @@ const NavLink: React.FC<NavLinkProps> = ({ url, disabled, title }) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [pathName]);
 
+	const staticClasses =
+		"uppercase text-white flex group translate-y-1 items-center transform-gpu justify-center font-transat-bold h-16 relative";
+	const cursorClasses = `${disabled ? "cursor-not-allowed" : "cursor-pointer"}`;
+	const disabledClasses = `${cursorClasses} ${disabled ? "opacity-50" : ""}`;
+
+	const linkClasses = `${staticClasses} ${disabledClasses}`;
+
 	return (
-		<NFCNavLink url={url} title={title} active={active} disabled={disabled} />
+		<Link href={!disabled ? url : ""} className={linkClasses}>
+			<div className="relative">
+				<div>
+					<span className={`relative ${cursorClasses}`}>{title}</span>
+				</div>
+				{!disabled && !active && (
+					<span className="absolute bottom-0 left-0 w-0 border-b-2 border-white transition-width duration-300 ease-out group-hover:w-full hover:w-full cursor-pointer" />
+				)}
+				{active && (
+					<span className="absolute bottom-0 left-0 w-full border-b-2 border-white" />
+				)}
+			</div>
+		</Link>
 	);
 };
 
