431cbcb58375261c545a6eb3a0af6655c020035b
Jamie Conway <jamie.c@nodecraft.com>
2024-06-24 07:28:51 -0500

    chore: remove unused commented lines

--- src/components/NewsletterForm.jsx
+++ src/components/NewsletterForm.jsx
@@ -1,8 +1,6 @@
 import ConvertKitForm from "./ConvertKitForm/ConvertKitForm";
 
 export default function NewsletterForm() {
-	// {/* <Script src="https://f.convertkit.com/ckjs/ck.5.js" /> */}
-	// {/*  action="https://app.convertkit.com/forms/3036294/subscriptions" */}
 	return (
 		<ConvertKitForm
 			scriptSrc="https://f.convertkit.com/ckjs/ck.5.js"
