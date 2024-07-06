2684fb2cb9de2d1824bf1c39021b3d0480940b4f
Jamie Conway <jamie.c@nodecraft.com>
2024-07-06 14:28:33 -0500

    feat: subscription confirmed page

---
+++ src/app/(pages)/confirm-subscription/page.tsx
@@ -0,0 +1,19 @@
+import FullWidthColorBackground from "@/components/FullWidthColorBackground";
+
+export default function Page() {
+	return (
+		<>
+			<main className="top-0 w-full flex flex-col items-center justify-start">
+				<FullWidthColorBackground variant="white" textColor="secondary">
+					<h2 className="font-transat-bold text-4xl text-center uppercase mb-16 text-darkgreen-500">
+						Subscription Confirmed!
+					</h2>
+					<p className="max-w-2xl text-balance">
+						Boom! You&apos;re officially confirmed and on the list. Expect some
+						great emails headed your way very soon.
+					</p>
+				</FullWidthColorBackground>
+			</main>
+		</>
+	);
+}
