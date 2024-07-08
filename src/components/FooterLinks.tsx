5c96f732a3d6c58a1b6fc5320813a31a32483d05
Jamie Conway <jamie.c@nodecraft.com>
2024-07-08 07:19:13 -0500

    fix: update footer link styles

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -1,55 +1,59 @@
 import Link from "next/link";
 
+const footerLinks = [
+	{
+		text: "About",
+		href: "/about",
+		id: "footer-link-about",
+	},
+	{
+		text: "Blog",
+		href: "/blog",
+		id: "footer-link-blog",
+	},
+	{
+		text: "Newsletter",
+		href: "/newsletter",
+		id: "footer-link-newsletter",
+	},
+	{
+		text: "Contact",
+		href: "/contact",
+		id: "footer-link-contact",
+	},
+	{
+		text: "Work With Us",
+		href: "/work-with-us",
+		id: "footer-link-work-with-us",
+	},
+	{
+		text: "Online Classes",
+		href: "/body-wisdom",
+		id: "footer-link-body-wisdom",
+	},
+	{
+		text: "Body Wisdom Mini-Course",
+		href: "https://my.practicebetter.io/#/6452605f4c15a8e1b30dd2df/bookings?c=6641804b6c0a3ac512999b29",
+		id: "footer-link-body-wisdom-mini-course",
+	},
+	{
+		text: "Book A Discovery Call",
+		href: "/call",
+		id: "footer-link-call",
+	},
+];
+
 const FooterLinks = () => (
 	<div className="font-transat-bold mt-6 flex flex-col sm:p-8 sm:flex-row flex-wrap items-top justify-center text-center text-xl text-brightgreen-500 uppercase gap-4 w-full max-w-7xl">
-		<Link
-			href="/about"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			About
-		</Link>
-		<Link
-			href="/blog"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			Blog
-		</Link>
-		<Link
-			href="/newsletter"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			Newsletter
-		</Link>
-		<Link
-			href="/contact"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			Contact
-		</Link>
-		<Link
-			href="/work-with-us"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			Work With Us
-		</Link>
-		<Link
-			href="/body-wisdom"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			Online Classes
-		</Link>
-		<Link
-			href="https://my.practicebetter.io/#/6452605f4c15a8e1b30dd2df/bookings?c=6641804b6c0a3ac512999b29"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			Body Wisdom Mini-Course
-		</Link>
-		<Link
-			href="/call"
-			className="border-b-2 border-b-brightgreen-500 hover:border-b-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
-		>
-			Book A Discovery Call
-		</Link>
+		{footerLinks.map((link) => (
+			<Link
+				key={link.id}
+				href={link.href}
+				className="underline decoration-2 underline-offset-4 decoration-brightgreen-500 hover:decoration-brightgreen-400 hover:text-brightgreen-400 uppercase text-nowrap"
+			>
+				{link.text}
+			</Link>
+		))}
 	</div>
 );
 
