8f7fd0139cc104b63f60bdb402ee7f3902c34485
Jamie Conway <jamie.c@nodecraft.com>
2024-07-22 21:17:44 -0500

    fix: types

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -1,29 +1,34 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
+	throw new Error(
+		"Please define the MONGODB_URI environment variable inside .env.local",
+	);
 }
 
-const uri = process.env.MONGODB_URI
-const options = {}
+const uri = process.env.MONGODB_URI;
+const options = {};
 
-let client
-let clientPromise: Promise<MongoClient>
+let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === 'development') {
-    // In development mode, use a global variable so that the value 
-    // is preserved across module reloads caused by HMR (Hot Module Replacement). 
-    if (!(global as any)._mongoClientPromise) {
-        const client = new MongoClient(uri, options)
-        ;(global as any)._mongoClientPromise = client.connect()
-    }
-    clientPromise = (global as any)._mongoClientPromise
+if (process.env.NODE_ENV === "development") {
+	// In development mode, use a global variable so that the value
+	// is preserved across module reloads caused by HMR (Hot Module Replacement).
+	const globalWithMongo = global as typeof globalThis & {
+		_mongoClientPromise?: Promise<MongoClient>;
+	};
+	if (!globalWithMongo._mongoClientPromise) {
+		const client = new MongoClient(uri, options);
+		globalWithMongo._mongoClientPromise = client.connect();
+	}
+	clientPromise = globalWithMongo._mongoClientPromise;
+
 } else {
-    // In production mode, it's best not to use a global variable.
-    const client = new MongoClient(uri, options)
-    clientPromise = client.connect()
+	// In production mode, it's best not to use a global variable.
+	const client = new MongoClient(uri, options);
+	clientPromise = client.connect();
 }
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
-export default clientPromise
+export default clientPromise;
