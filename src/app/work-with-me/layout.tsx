b2734ed792c41f6e45996f63e222e2f10962d62a
Jamie Conway <jamie.c@nodecraft.com>
2024-07-22 21:03:38 -0500

    fix: button link and size

--- src/app/work-with-me/layout.tsx
+++ src/app/work-with-me/layout.tsx
@@ -12,8 +12,13 @@ export default function RootLayout({
 				<header className="sticky top-0 w-full z-40 bg-opacity-50 dark:bg-opacity-50 bg-bluegreen-500 dark:bg-bluegreen-700 backdrop-blur-xl dark:backdrop-blur-xl transition-all duration-300">
 					<div className="px-2 sm:px-7 flex flex-row items-center justify-between max-w-7xl h-16 m-auto">
 						<NFCLogo imgUrl="/images/NOURISHED_LOGO_NOTAG_WHITE.png" />
-						<div className="h-full flex items-center max-w-full">
-							<NFCButton href="#" as="a" variant="tertiary" classes="h-12">
+						<div className="h-full flex items-center justify-end max-w-full scale-75 sm:scale-90">
+							<NFCButton
+								href="/apply"
+								as="a"
+								variant="tertiary"
+								classes="h-12 text-nowrap"
+							>
 								APPLY NOW
 							</NFCButton>
 						</div>
