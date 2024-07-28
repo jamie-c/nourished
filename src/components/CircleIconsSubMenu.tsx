294bef52fd2f6e8fb7505816186e9518e87d37b1
Jamie Conway <jamie.c@nodecraft.com>
2024-07-27 21:51:08 -0500

    feat: ui circle nav icons

--- src/components/CircleIconsSubMenu.tsx
+++ src/components/CircleIconsSubMenu.tsx
@@ -1,6 +1,5 @@
 import type { VariantsOptions } from "@nourishedco/ui";
-// import { NFCCircle } from "@nourishedco/ui";
-import Image from "next/image";
+import { NFCCircle } from "@nourishedco/ui";
 import Link from "next/link";
 
 type Nav = {
@@ -30,7 +29,7 @@ const navs: Nav[] = [
 		title: "Newsletter",
 	},
 	{
-		page: "work-with-us",
+		page: "work-with-me",
 		icon: "clipboard",
 		variant: "darkred",
 		title: "Work With Me",
@@ -38,21 +37,15 @@ const navs: Nav[] = [
 ];
 
 const CircleIconsSubMenu = () => (
-	<div className="flex flex-col md:flex-row items-center justify-evenly w-full py-28 md:py-32 max-w-7xl mx-auto">
+	<div className="flex flex-col sm:grid sm:grid-cols-2 sm:gap-8 lg:flex lg:flex-row items-center justify-evenly gap-4 w-full my-28 max-w-7xl mx-auto">
 		{navs.map((nav) => (
 			<Link key={`${nav.page}-link`} href={`/${nav.page}`}>
-				{/* <NFCCircle
+				<NFCCircle
 					size="3xl"
 					variant={nav.variant}
 					title={nav.title}
-					imageUrl={`/images/icons/${nav.icon}`}
-				/> */}
-				<Image
-					className="py-6 md:py-0 px-24 sm:px-6 md:px-4 lg:px-10 w-auto h-auto"
-					src={`/images/circle-icon-${nav.page}.png`}
-					width={200}
-					height={200}
-					alt="Nourished"
+					imageUrl={`/images/icons/${nav.icon}.png`}
+					classes="shrink m-auto"
 				/>
 			</Link>
 		))}
