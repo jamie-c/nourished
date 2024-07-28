5400a04aea29e4344b426c6a3219a9f887f93e0a
Jamie Conway <jamie.c@nodecraft.com>
2024-07-27 20:34:11 -0500

    feat: redirects

--- next.config.js
+++ next.config.js
@@ -1,13 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+            // Basic redirect
+            {
+                source: "/confirm-fxfj-subscription",
+                destination: "/confirm-waitlist",
+                permanent: true,
+            },
+            // Wildcard path matching
+            // {
+            //     source: "/blog/:slug",
+            //     destination: "/news/:slug",
+            //     permanent: true,
+            // },
+        ]
+    },
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com',
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
             },
         ],
     },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
