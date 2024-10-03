a88f70ca9c7d078a69dedcd64872392a1d4a29e0
Jamie Conway <jamie.c@nodecraft.com>
2024-10-03 12:42:15 -0500

    fix: subscribe form

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,30 +1,32 @@
-import type { NFCSubscribeFormData } from "@/types/subscribe-form";
-import { validateFormData } from "@/types/subscribe-form";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import type { NFCSubscribeFormData } from "@/types/subscribe-form"
+import { validateFormData } from "@/types/subscribe-form"
+import type { NextRequest } from "next/server"
+import { NextResponse } from "next/server"
 
 export async function POST(request: NextRequest) {
-    const formData: NFCSubscribeFormData = await request.json();
+    const formData: NFCSubscribeFormData = await request.json()
 
-    const {isValid, errors, data} = validateFormData(formData);
+    const { isValid, errors, data } = validateFormData(formData)
 
     if (!isValid) {
-        return NextResponse.json({ message: errors?.[0] ?? "Invalid input", code: "error.application.invalid" });
+        return NextResponse.json({
+            message: errors?.[0] ?? "Invalid input",
+            code: "error.application.invalid",
+        })
     }
-    const body = await request.json()
     const api_key = process.env.CONVERTKIT_API_KEY
-    const api_secret = process.env.CONVERTKIT_API_SECRET
+    // const api_secret = process.env.CONVERTKIT_API_SECRET
 
     try {
         const results = await fetch(
-            `https://api.convertkit.com/v3/forms/${body.dataForm}/subscribe`,
+            `https://api.convertkit.com/v3/forms/${formData.dataForm}/subscribe`,
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json; charset=utf-8" },
                 body: JSON.stringify({
                     api_key,
-                    email: body.email,
-                    first_name: body?.first_name,
+                    email: formData.email,
+                    first_name: formData?.first_name,
                 }),
             }
         )
